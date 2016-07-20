4f80bc4daac8fc86a935f5381fdb283463e213aa
Sarah DeBey <sarah.debey@gmail.com>
2016-07-19 19:59:29 -0700

    data pull

---
+++ scripts/markets.js
@@ -0,0 +1,74 @@
+(function(module) {
+
+  function Markets (opts) {
+    for (keys in opts) {
+      this[keys] = opts[keys];
+    };
+  };
+
+  Markets.all = [];
+
+  //function to get the data from local database if it exists, if not, get from api
+  Markets.getData = function() {
+    console.log('in getData');
+    webDB.execute('SELECT * from marketdata', function(rows) {
+      console.log('in getData 2');
+      if (rows.length) {
+        console.log('in getData, rows exist');
+        Markets.all = rows;
+      } else {
+        console.log('in getData, rows do not exist');
+        $.ajax({
+          type: 'GET',
+          contentType: "application/json; charset=utf-8",
+          url: 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=98103',
+          dataType: 'jsonp',
+          jsonpCallback: 'Markets.getFromApi',
+        });
+      }
+    });
+  };
+
+  //function to create the websql table, then call the get data after created
+  Markets.createTable = function(next) {
+    console.log('in createTable');
+    webDB.execute(
+      'CREATE TABLE IF NOT EXISTS marketdata (' +
+      'id INTEGER PRIMARY KEY, ' +
+      'marketname VARCHAR(255);'
+    );
+    Markets.getData();
+  };
+
+  //function to insert Markets record into websql table
+
+  Markets.prototype.insertRecord = function() {
+    console.log('in insertRecord');
+    webDB.execute(
+      [
+        {
+          'sql': 'INSERT INTO marketdata(id, marketname) VALUES (?, ?);',
+          'data': [this.results.id, this.results.marketname],
+        }
+      ]
+    );
+  };
+
+  Markets.getFromApi = function(data) {
+    console.log('in getFromApi');
+    Markets.all = data;
+    if (!rows.length) {
+      Markets.all.results.forEach(function(singleMarket) {
+        var market = new Markets(singleMarket);
+        market.insertRecord();
+      });
+    }
+    webDB.execute('SELECT * from marketdata', function(rows) {
+      console.log('oh hai');
+    });
+  };
+
+  Markets.createTable();
+
+  module.Markets = Markets;
+})(window);
